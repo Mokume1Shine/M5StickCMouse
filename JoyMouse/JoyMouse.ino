6990871d178c49f34294b24027746743f29f8f6f
Mokume1Shine <tuetuti100@gmail.com>
2022-02-03 13:13:16 +0900

    Update JoyMouse.ino

--- JoyMouse/JoyMouse.ino
+++ JoyMouse/JoyMouse.ino
@@ -41,15 +41,18 @@ void setup() {
   //ジョイスティックとの接続開始
   Wire.begin(0, 26, 100000);
 
-  //少電力設定開始
+  //省電力設定開始
   M5.Axp.ScreenBreath(10);
   M5.Lcd.setSwapBytes(true);
   canvas.createSprite(M5.Lcd.width(),M5.Lcd.height());
   canvas.setSwapBytes(false);
 }
 
+//以降ここをループ
 void loop() {
+  //加速度センサー情報取得
   M5.MPU6886.getAccelData(&accX,&accY,&accZ);
+  //ジョイスティックからの情報取得開始
   Wire.beginTransmission(JOY_ADDR);
   Wire.write(0x02);
   Wire.endTransmission();
@@ -59,26 +62,29 @@ void loop() {
     joyY=Wire.read();
     joyB=Wire.read();
   }
-  
+
+  //マウスカーソルの移動
   mouse_x=0;
   mouse_y=0;
-  if(abs(accX*1000)>mouse_min){
-    mouse_x=-accX*mouse_senseX;
-  }
-  if(abs(accY*1000)>mouse_min){
-    mouse_y=accY*mouse_senseY;
+
+  //本体の傾きによる移動をロック
+  if(!lock){
+    if(abs(accX*1000)>mouse_min){
+      mouse_x=-accX*mouse_senseX;
+    }
+    if(abs(accY*1000)>mouse_min){
+      mouse_y=accY*mouse_senseY;
+    }
   }
 
-  if(lock){
-    mouse_x=0;
-    mouse_y=0;
-  }
-  
+  //ジョイスティックによるカーソル移動
   if(abs(joyX)>=5)mouse_x+=map(joyX,-110,110,-10,10);
   if(abs(joyY)>=5)mouse_y+=map(joyY,-110,110,10,-10);
   bleMouse.move(mouse_x,mouse_y);
-  
+
   M5.update();
+  //マウスボタン情報の送信
+  //マウスのボタンを押すときと離すとき，それぞれのタイミングで送信する必要がある
   pressB=0;
   releaseB=0;
   if(joyB==0){
@@ -93,20 +99,23 @@ void loop() {
   }
   if(pressB!=0)bleMouse.press(pressB);
   if(releaseB!=0)bleMouse.release(releaseB);
+  //ロックボタンの処理
   if(M5.BtnB.isPressed() && !pLockButton){
     lock=!lock;
   }
   pLockButton=M5.BtnB.isPressed();
 
+  //バッテリ情報の取得
   float vbat=M5.Axp.GetBatVoltage();
   float cbat=M5.Axp.GetBatCurrent();
 
+  //画面表示の処理
   M5.Lcd.startWrite();
   canvas.fillRect(0,0,80,160,BLACK);
   canvas.drawRect(10,10,60,60,WHITE);
   canvas.fillRect(40,40,2,2,WHITE);
   canvas.fillRect(40+mouse_x-1,40+mouse_y-1,4,4,0xFC40);
-  
+
   if(lock){
     canvas.pushImage(73,76,LockWidth,LockHeight,Lock);
   }else{
@@ -123,17 +132,20 @@ void loop() {
     canvas.pushImage(42,80,RightButtonWidth,RightButtonHeight,RightButton);
   }
   canvas.pushImage(26,106,MouseBottomWidth,MouseBottomHeight,MouseBottom);
-  
+
   canvas.pushImage(9,138,BatteryWidth,BatteryHeight,Battery);
   if(cbat>=0){
     canvas.pushImage(0,142,PlugLeftWidth,PlugLeftHeight,PlugLeft);
     canvas.pushImage(76,142,PlugRightWidth,PlugRightHeight,PlugRight);
   }
 
+  //バッテリ残量のバー表示
   canvas.fillRect(12,141,(int16_t)constrain(map(vbat*100.0,310,390,0,57),0,57),10,WHITE);
-  
+
+  //画面出力
   canvas.pushSprite(0,0);
   M5.Lcd.endWrite();
-  
+
+  //気持ちだけ処理を止める
   delay(10);
 }
